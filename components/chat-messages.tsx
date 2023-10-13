5111dfb106adbe45ca10f18f5c938a025bd86fa9
Ayas Oukache <ayasouk.iam@gmail.com>
2023-10-14 01:55:21 +0200

    Memory Management

--- components/chat-messages.tsx
+++ components/chat-messages.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Mentor, Message } from "@prisma/client";
 import { ChatMessage, ChatMessageProps } from "@/components/chat-message";
-import { useEffect, useState } from "react";
+import { ElementRef, useEffect, useRef, useState } from "react";
 
 interface ChatMessagesProps {
     mentor: Mentor;
@@ -9,22 +9,44 @@ interface ChatMessagesProps {
     isLoading: boolean;
 }
 
-const ChatMessages = ({mentor, messages=[], isLoading}:ChatMessagesProps) => {
+const ChatMessages = ({mentor, messages, isLoading}:ChatMessagesProps) => {
+    const scrollRef = useRef<ElementRef<"div">>(null);
+
     const [fakeLoading, setFakeLoading] = useState(messages.length === 0 ? true : false);
 
-    useEffect(()=>{
+    useEffect(() => {
         const timeout = setTimeout(()=>{
             setFakeLoading(false);
         }, 1000);
 
-        return ()=>{
+        return () => {
             clearTimeout(timeout);
         }
     }, []);
 
+    useEffect(()=>{
+        scrollRef?.current?.scrollIntoView({ behavior: "smooth"});
+    }, [messages.length]);
+
     return (
         <div className="flex-1 overflow-y-auto pr-4">
-            <ChatMessage src={mentor.src} role="system" content={`Hello, I am ${mentor.name}, ${mentor.description} `}/>
+            <ChatMessage isLoading={fakeLoading} src={mentor.src} role="system" content={`Hello, I am ${mentor.name}, ${mentor.description} `}/>
+            {messages.map((message) => (
+                <ChatMessage 
+                    key={message.content}
+                    role={message.role}
+                    content={message.content}
+                    src={message.src}
+                />
+            ))}
+            { isLoading && (
+                <ChatMessage
+                    role="system"
+                    src={mentor.src}
+                    isLoading
+                />
+            )}
+            <div ref={scrollRef}/>
         </div>
     )
 }
---
+++ lib/memory.ts
@@ -0,0 +1,120 @@
+import { Redis } from "@upstash/redis";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { PineconeClient } from "@pinecone-database/pinecone";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
+
+export type MentorKey =  {
+    mentorName: string;
+    modelName: string;
+    userId: string;
+}
+
+export class MemoryManager {
+    private static instance: MemoryManager;
+    private history: Redis;
+    private vectorDBClient: PineconeClient;
+
+    public constructor(){
+        this.history = Redis.fromEnv();
+        this.vectorDBClient = new PineconeClient();
+    }
+
+    public async init() {
+        if(this.vectorDBClient instanceof PineconeClient) {
+            await this.vectorDBClient.init({
+                apiKey: process.env.PINECONE_API_KEY!,
+                environment: process.env.PINECONE_ENVIRONMENT!,
+            });
+        }
+    }
+
+    public async vectorSearch(
+        recentChatHistory: string,
+        mentorFileName: string
+    ) {
+        const pineconeClient = <PineconeClient>this.vectorDBClient;
+
+        const pineconeIndex = pineconeClient.Index(
+            process.env.PINECONE_INDEX! || ""
+        )
+
+        const vectorStore = await PineconeStore.fromExistingIndex(
+            new OpenAIEmbeddings({ openAIApiKey: process.env.OPENAI_API_KEY}),
+            {pineconeIndex}
+        );
+
+        const similarDocs = await vectorStore.similaritySearch(recentChatHistory, 3, { fileName: mentorFileName})
+                                            .catch((err)=>{
+                                                console.log("Failed to get vector search results", err);
+                                            });
+
+        return similarDocs;
+    }
+
+    public static async getInstance(): Promise<MemoryManager> {
+        if(!MemoryManager.instance) {
+            MemoryManager.instance = new MemoryManager();
+            await MemoryManager.instance.init();
+        }
+
+        return MemoryManager.instance;
+    }
+
+
+    private generateRedisMentorKey(mentorKey: MentorKey): string {
+        return `${mentorKey.mentorName}-${mentorKey.modelName}-${mentorKey.userId}`
+    }
+
+    public async writeHistory(text: string, mentorKey: MentorKey) {
+        if(!mentorKey || typeof mentorKey.userId == "undefined") {
+            console.log("Mentor key set incorrectly");
+            return "";
+        }
+
+        const key = this.generateRedisMentorKey(mentorKey);
+        const result = await this.history.zadd(key, {
+            score: Date.now(),
+            member: text,
+        });
+
+        return result;
+    }
+
+    public async readLatestHistory(mentorKey: MentorKey): Promise<string> {
+        if(!mentorKey || typeof mentorKey.userId == "undefined") {
+            console.log("mentor key set incorrectly");
+            return "";
+        }
+
+        const key = this.generateRedisMentorKey(mentorKey);
+        let result = await this.history.zrange(key, 0, Date.now(), {
+            byScore: true,
+        });
+
+        result = result.slice(-30).reverse();
+        const recentChats = result.reverse().join("\n");
+        return recentChats;
+    }
+
+    public async seedChatHistory(
+        seedContent: string,
+        delimiter: string = "\n",
+        mentorKey: MentorKey
+    ) {
+        const key = this.generateRedisMentorKey(mentorKey);
+
+        if(await this.history.exists(key)) {
+            console.log("user already has chat history");
+            return;
+        }
+
+        const content = seedContent.split(delimiter);
+        let counter = 0;
+
+        for (const line of content) {
+            await this.history.zadd(key, { score: counter, member: line});
+            counter +=1;
+        }
+    }
+
+}
